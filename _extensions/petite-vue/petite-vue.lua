6979a1f5b46a62f2378bc3e65801b5b906b570c1
Elian H. Thiele-Evans <60372411+ElianHugh@users.noreply.github.com>
2023-01-25 12:43:57 +1100

    WIP allow v-on and v-bind in hashpipe
    v-on and v-bind are treated as nested objects
    
    Need to ensure that ternary operators will work later down the road

--- _extensions/petite-vue/petite-vue.lua
+++ _extensions/petite-vue/petite-vue.lua
@@ -14,3 +14,48 @@ if (kIsHTML) then
     }
   })
 end
+
+local function createKVPairs(inputstr)
+  inputstr = string.gsub(inputstr, "[{}]", "")
+  ---@type table<number, string>
+  local rows = {}
+  ---@type table<string, string>
+  local kvPairs = {}
+  --- Split string into rows separated by commas
+  for str in string.gmatch(inputstr, "([^,]+)") do
+    table.insert(rows, str)
+  end
+  --- Split rows into key value pairs separated by colons
+  for _, v in pairs(rows) do
+    ---@type table<string>
+    local x, y = string.match(v, "(.+):(.+)");
+    kvPairs[x] = y;
+  end
+  return kvPairs;
+end
+
+---Fixes attributes that are of the form foo:bar:baz
+---@param div pandoc.Div
+---@return pandoc.Div
+local function fixColonAttributes(div)
+  local vbind = div.attributes["v-bind"];
+  local von = div.attributes["v-on"];
+  if (vbind) then
+    local values = createKVPairs(vbind)
+    for k, v in pairs(values) do
+      div.attributes["v-bind:" ..  string.gsub(k, '"', "")] = string.gsub(v, '"', "");
+    end
+  end
+  if (von) then
+    local values = createKVPairs(von)
+    for k, v in pairs(values) do
+      div.attributes["v-on:" .. string.gsub(k, '"', "")] = string.gsub(v, '"', "");
+    end
+  end
+  return div;
+end
+
+
+function Div(div)
+  return fixColonAttributes(div);
+end
